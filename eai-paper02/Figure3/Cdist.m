471a77c195806e538cbeabc5ab05cefce61b7a94
joaquin <joaquin@HaliBookPro.local>
2017-06-23 21:55:55 +0200

    renaming files

--- eai-paper02/Figure3/Cdist.m
+++
@@ -1,9 +0,0 @@
-function [ d ] = Cdist( P )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-T1=power(P(1,1)-P(2,1),2);
-T2=power(P(1,2)-P(2,2),2);
-T3=power(P(1,3)-P(2,3),2);
-d=sqrt(T1+T2+T3);
-end
-
--- eai-paper02/Figure5/SmoothValues.m
+++
@@ -1,7 +0,0 @@
-function [ PS ] = SmoothValues( P,R )
-%Create new smoothed array by adding values in between
-L=length(P);
-Xi = 1:1/R:L;
-P1 = pchip(1:L,P(1:L,1),Xi)';
-P2 = pchip(1:L,P(1:L,2),Xi)';
-PS=[P1(1:(L*R/2)) P2(1:(L*R/2))];
